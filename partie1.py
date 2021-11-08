56c39e09ecdecffe975244393978aa36135358f4
Junior <gjunior02@outlook.com>
2021-11-08 11:56:11 +0100

    partie1 V1.0 wip

--- partie1.py
+++ partie1.py
@@ -1,44 +1,29 @@
-def main(n):
-    board = [[0] * (n + 3) for i in range(n + 3)]
+def init_board(n : int):
+    board = [[0] * (n) for i in range(n)]
+    for line in (board[0], board[1]):
+        for numbers in range((len(line))):
+                line[numbers] = 2
 
-    def board_mat(x):
-        for line in (x[0], x[len(x) - 1]):
-            for numbers in range((len(line))):
-                if numbers in (0, 1, len(line) - 2, len(line) - 1):
-                    line[numbers] = ''
+    for line in (board[n - 2], board[n-1]):
+        for numbers in range((len(line))):
+                line[numbers] = 1
+    return board
+def print_board(board : list[list[int]]):
 
-        for line in (x[1], x[2]):
-            for numbers in range((len(line))):
-                if numbers in (0, 1, len(line) - 1):
-                    line[numbers] = 0
-                else:
-                    line[numbers] = 2
+    res=""
+    for line in board:
+        for i in range(len(line)):
+            if line[i] == 2:
+                res += 'B '
+            elif line[i] == 0:
+                res += '. '
+            elif line[i] == 1:
+                res += 'W '
+        res +="\n"
+    return res
 
-        for line in (x[n - 1], x[n]):
-            for numbers in range((len(line))):
-                if numbers in (0, 1, len(line) - 1):
-                    line[numbers] = 0
-                else:
-                    line[numbers] = 1
-        return x
-
-    def print_board(x):
-        res = ""
-        for line in x:
-            for i in range(len(line)):
-                if line[i] == 2:
-                    res += 'B '
-
-                elif line[i] == 0:
-                    res += '. '
-
-                else:
-                    res += 'W '
-            res += "\n"
-
-        return res
-
-    return print_board(board_mat(board))
+print(init_board(8))
+print(print_board(init_board(8)))
 
 
-print(main(8))
+
